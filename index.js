c5c2eb061d3cc4cdab370c7152b09903e7cfb49e
nyohoki <nyohoki@gmail.com>
2021-09-27 05:01:47 +0900

    deploy command added

--- index.js
+++ index.js
@@ -10,5 +10,19 @@ client.once('ready', () => {
     console.log('tsukaima is ready!');
 });
 
-// login to discord with your client's token
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
+// login to discord with your client's token, this needs to be last
 client.login(token);
