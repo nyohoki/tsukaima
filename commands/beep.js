a45bc875e62128a94a9d4eab5207636b185735a5
nyohoki <nyohoki@gmail.com>
2021-09-27 09:14:07 +0900

    test

--- commands/beep.js
+++ commands/beep.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('beep')
 		.setDescription('Replies with boop'),
 	async execute(interaction) {
 		await interaction.reply('boop');
+		await interaction.deleteReply();
 	},
 };
