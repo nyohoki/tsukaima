3fd5e4ddd36945a81138ebf0152cacb73571363d
nyohoki <nyohoki@gmail.com>
2023-12-22 13:24:10 -0800

    update

--- jsbot/commands/fun/sockes.js
+++ jsbot/commands/fun/sockes.js
@@ -2,7 +2,7 @@ const {Client, Collection, GatewayIntentBits} = require('discord.js');
 const client = new Client({intents: 32767});
 client.commands = new Collection();
 client.on("messageCreate", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+  if (message.content.startsWith("slut")) {
+    message.react("😄");
   }
 })
